a70aaf322ad9d91a90094d9dd2ce7fd0bd0a219f
p0bailey <phillip@bailey.st>
2017-07-25 16:45:23 +0100

    sgs to from list to string

--- variables.tf
+++ variables.tf
@@ -16,11 +16,11 @@ variable "public_ip" {
   default = "false"
 }
 
-variable "sgs" {}
 
-#variable "sgs" {
-#  type = "list"
-#}
+
+variable "sgs" {
+  type = "list"
+}
 
 
 variable "user_data" {}
